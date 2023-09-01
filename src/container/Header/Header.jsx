9765b679a55d1c37ffa3b395e37cb236862acf2a
Sander Vinter <sander.vinter@gmail.com>
2023-09-01 13:48:57 +0300

    Restaurant page with React.JS

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Header.css";
+
+const Header = () => (
+  <div className="app__header app__wrapper section__padding" id="#home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Indulge in a culinary journey at our restaurant, where every dish is a
+        masterpiece. From savory starters to decadent desserts, our menu
+        celebrates flavors from around the world, crafted with the freshest
+        ingredients to satisfy your palate's desires.
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header_img" />
+    </div>
+  </div>
+);
+
+export default Header;
